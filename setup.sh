a6d81ef0f7263c70bf1d145729f6a78b67f2983c
Matt Willsher <matt.willsher@boxuk.com>
2012-04-17 12:19:27 +0100

    setup.sh: Fixed to install vunlde and bundles if not present

--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Make sure vundle is present 
-if [[ ! -e $HOME/.vim/bundle/vundle/README.md ]]; then
-  cd $HOME/.vim
-  git submodule init && git submodule update
-fi
-
 # Symlink vimrc
 if [[ ! -f $HOME/.vimrc ]]; then 
   # Get the full path of the vimrc 
   cd $( dirname $0 ) 
-  [[ ! -f ${PWD}/vimrc ]] && echo "${PWD}/vimrc is missing. Nothing to symlink." >&2 && exit 1
+  [[ ! -f ${PWD}/vimrc ]] && echo "${PWD}/vimrc is missing" >&2 && exit 1
   ln -s ${PWD}/vimrc $HOME/.vimrc
-else
-  echo $HOME/.vimrc already exists >&2
-  exit 1
 fi
 
+# Make sure vundle is present 
+if [[ ! -d $HOME/.vim/bundle/vundle/.git ]]; then
+  echo Installing vunlde and installing bundles
+  cd $HOME/.vim
+  git submodule init && git submodule update
+  vim +BundleInstall +qall
+fi
